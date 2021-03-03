884de97c2ed180a8e6b5f88f0d47c755ba71ff0a
ManuelRojasR <m.rojas@lifeware.cl>
2021-03-03 17:25:29 -0300

    nombre significativo

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    IntegraBoard: ['intro','descargar-y-descomprimir-pictogramas','importar-pictogramas-a-integraboard'],
+    IntegraBoard: ['manual-integraboard','descargar-y-descomprimir-pictogramas','importar-pictogramas-a-integraboard'],
     OpenEyes: ['manual-openeyes'],
     'Tutoriales generales': ['modem-huawei'],
   },
