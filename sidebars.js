05eabbed5299ef27bcbe7102d40c7d056da66d7c
freyes <f.reyes@lifeware.cl>
2021-05-05 16:33:26 -0400

    manual-lifeware-integra

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   someSidebar: {
     IntegraBoard: ['manual-integraboard','descargar-y-descomprimir-pictogramas','importar-pictogramas-a-integraboard'],
     OpenEyes: ['manual-openeyes'],
+    LifewareIntegra: ['manual-lifeware-integra'],
     'Tutoriales generales': ['modem-huawei'],
   },
 };
