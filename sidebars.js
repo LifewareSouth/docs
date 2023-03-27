62c6b3ff4933e857abf241c81195782919641ba3
Gonza <g.jaques@lifeware.cl>
2023-03-27 11:36:22 -0300

    cambio de nombre y  se agrega al slidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    IntegraBoard: ['manual-integraboard','descargar-y-descomprimir-pictogramas','importar-pictogramas-a-integraboard','manual-voces','manual-voces-aholab'],
+    IntegraBoard: ['manual-integraboard','descargar-y-descomprimir-pictogramas','importar-pictogramas-a-integraboard','manual-voces','manual-voces-aholab','manual-integraboard4.md'],
     OpenEyes: ['manual-openeyes'],
     LifewareIntegra: ['manual-lifeware-integra','configuracion-teclado-lifeware-integra'],
     'Tutoriales Generales': ['modem-huawei', 'problemas-huawei']
